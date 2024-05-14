d43eb2f1984b1ddd8a157087c2cea38bd9be1112
marinhoisa <106539700+marinhoisa@users.noreply.github.com>
2024-05-14 16:03:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -23,15 +23,6 @@ const tempoObjetivo4 = new Date("2024-05-24T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-function atualizaCronometro(){
-    for (let  i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
-    }
-}
-
-atualizaCronometro();
-
-setInterval(atualizaCronometro(),1000);
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
@@ -42,11 +33,27 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-
+    
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if(tempoFinal>0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else{
+    return "Prazo Finalizado";
 }
+}
+
+
+function atualizaCronometro(){
+    for (let  i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+  function comecaCronometro() {
+      atualizaCronometro();
+      
+      setInterval(atualizaCronometro,1000);
+}
+  comecaCronometro();
 
